f6c8fd79b32b0650c30b6607a3e507555df3c8eb
João Silva <pepar_jp33@hotmail.com>
2020-09-02 22:35:24 +0100

    Updates

---
+++ precache-manifest.bc250b09bed700a5ccef49fcfb5bdc60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "78ef62e2ce242c65ae112e77b9459118",
+    "url": "/react-movie-tvshow-app/index.html"
+  },
+  {
+    "revision": "77573127c1875a8b1212",
+    "url": "/react-movie-tvshow-app/static/css/main.953364bb.chunk.css"
+  },
+  {
+    "revision": "40eaa189bd4d569dc199",
+    "url": "/react-movie-tvshow-app/static/js/2.85eb12da.chunk.js"
+  },
+  {
+    "revision": "8859687eef945b55b9de4bad693044d6",
+    "url": "/react-movie-tvshow-app/static/js/2.85eb12da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77573127c1875a8b1212",
+    "url": "/react-movie-tvshow-app/static/js/main.30ada716.chunk.js"
+  },
+  {
+    "revision": "72398fb37b57be34db1a",
+    "url": "/react-movie-tvshow-app/static/js/runtime-main.312d785f.js"
+  }
+]);
